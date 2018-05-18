363a66bece9a5dbd8545363b9e809c6f90d517ab
jireh14 <37375957+jireh14@users.noreply.github.com>
2018-05-18 00:16:11 -0500

    Add files via upload

---
+++ TIPOS DE DATOS.java
@@ -0,0 +1,21 @@
+public class My class
+public static void main (String[] args) {
+    int min = -2147483648;
+    int max = 2147483647;
+    byte min = -128;
+    byte max = 127;
+    short min = -32768;
+    short max = 32767;
+    long min = -922337203;
+    long max = 922337203;
+    float min = (float) -34.10;
+    float max = (float) 34.10;
+    System.out.println("tipo\tminimo\tmaximo");
+    System.out.println("int\t" + integer.MIN_VALUE + "\t" + integer.MAX_VALUE);
+    System.out.println("byte\t" + byte.MIN_VALUE + "\t" + byte.MAX_VALUE);
+    System.out.println("short\t" + short.MIN_VALUE + "\t" + short.MAX_VALUE);
+    System.out.println("long\t" + long.MIN_VALUE + "\t" + long.MAX_VALUE);
+    System.out.println("float\t" + float.MIN_VALUE + "\t" + float.MAX_VALUE);
+    
+
+  
